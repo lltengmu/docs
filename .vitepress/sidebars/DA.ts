01a85450c5f0ad9beccadc3fcbacb64773164a7c
eddie <1150970484@qq.com>
2025-01-08 23:24:03 +0800

    update 数据结构数组

--- .vitepress/sidebars/DA.ts
+++ .vitepress/sidebars/DA.ts
@@ -4,13 +4,9 @@ export default {
       text: '数据结构与算法',
       items: [
         {
-          text: '初识算法',
-          link: '/internal-strength/Data-Structures-And-Algorithms/01.数据结构与算法简介',
-        },
-        {
-          text: '复杂度分析',
-          link: '/internal-strength/Data-Structures-And-Algorithms/02.复杂度分析.md',
-        },
+          text: '数组',
+          link: '/internal-strength/Data-Structures-And-Algorithms/Code-random-recording/01.数组',
+        }
       ],
     },
   ],
