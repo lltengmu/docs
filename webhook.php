19892eaf66b86e6dbf20418be54348b088a0f6d3
eddie <1150970484@qq.com>
2025-01-09 22:51:14 +0800

    备份自动部署文件

--- webhook.php
+++ webhook.php
@@ -13,9 +13,14 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
     $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
     if (strcmp($signature, $hash) == 0) {
-        shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        // $output = shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        $command = "cd $path && /usr/bin/git pull";
+        $output = [];
+        $return_var = null;
+        exec($command, $output, $return_var);
         exit();
     }
 }
 
 http_response_code(404);
+
