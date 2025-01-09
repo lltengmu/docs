6a17ada5983aff964519d62eec919c0c12c2955d
eddie <1150970484@qq.com>
2025-01-09 22:41:39 +0800

    更新自动部署文件

--- public/webhook.php
+++ public/webhook.php
@@ -1,6 +1,31 @@
 <?php
 // GitHub Webhook Secret.
 // GitHub项目 Settings/Webhooks 中的 Secret
+// $secret = "Eddie-docs";
+
+// // Path to your respostory on your server.
+// // e.g. "/var/www/respostory"
+// // 项目地址
+// $path = "/www/wwwroot/docs.hub-sky.com/docs";
+// // Headers deliveried from GitHub
+// $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+// if ($signature) {
+//     $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+//     if (strcmp($signature, $hash) == 0) {
+//         // $output = shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+//         $command = "cd $path && /usr/bin/git pull";
+//         $output = [];
+//         $return_var = null;
+//         exec($command, $output, $return_var);
+//         exit();
+//     }
+// }
+
+// http_response_code(404);
+
+// GitHub Webhook Secret.
+// GitHub项目 Settings/Webhooks 中的 Secret
 $secret = "Eddie-docs";
 
 // Path to your respostory on your server.
@@ -13,13 +38,10 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
     $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
     if (strcmp($signature, $hash) == 0) {
-        // $output = shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-        $command = "cd $path && /usr/bin/git pull";
-        $output = [];
-        $return_var = null;
-        exec($command, $output, $return_var);
+        shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
         exit();
     }
 }
 
 http_response_code(404);
+
