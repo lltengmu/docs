12b4e85bb7e75ec854d563722bb2724af2403ca5
eddie <1150970484@qq.com>
2025-01-09 22:10:00 +0800

    更新自动部署文件

--- public/webhook.php
+++ public/webhook.php
@@ -17,7 +17,8 @@ if ($signature) {
     $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
     if (strcmp($signature, $hash) == 0) {
         file_put_contents($log, "执行指令" . "\n", FILE_APPEND);
-        $output = shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        // $output = shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        $output = shell_exec("cd {$path} && /usr/bin/git pull");
         file_put_contents($log, "指令执行结果:" . $output . "\n", FILE_APPEND);
         exit();
     }
