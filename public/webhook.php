d01daa90a14a72699e5468d842f8e08e0279d25d
eddie <1150970484@qq.com>
2025-01-09 20:44:51 +0800

    更新自动部署文件

--- public/webhook.php
+++ public/webhook.php
@@ -7,16 +7,21 @@ $secret = "Eddie-docs";
 // e.g. "/var/www/respostory"
 // 项目地址
 $path = "/www/wwwroot/docs";
-`cat 1.html >> a.txt`;
-// Headers deliveried from GitHub
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 
-if ($signature) {
-    $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
-    if (strcmp($signature, $hash) == 0) {
-        echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-        exit();
+$log = "/www/wwwroot/docs/public/a.txt";
+// Headers deliveried from GitHub
+try {
+    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+    if ($signature) {
+        $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+        if (strcmp($signature, $hash) == 0) {
+            echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+            exit();
+        }
     }
+} catch (\Throwable $th) {
+    file_put_contents($log, $th->getMessage(), FILE_APPEND);
 }
 
 http_response_code(404);
--- webhook.php
+++ webhook.php
@@ -8,15 +8,20 @@ $secret = "Eddie-docs";
 // 项目地址
 $path = "/www/wwwroot/docs";
 
+$log = "/www/wwwroot/docs/public/a.txt";
 // Headers deliveried from GitHub
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+try {
+    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 
-if ($signature) {
-    $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
-    if (strcmp($signature, $hash) == 0) {
-        echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-        exit();
+    if ($signature) {
+        $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+        if (strcmp($signature, $hash) == 0) {
+            echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+            exit();
+        }
     }
+} catch (\Throwable $th) {
+    file_put_contents($log, $th->getMessage(), FILE_APPEND);
 }
 
 http_response_code(404);
