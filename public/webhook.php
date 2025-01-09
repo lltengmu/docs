5fac1468be17abd085f9090714144b5a5a813411
eddie <1150970484@qq.com>
2025-01-09 21:46:12 +0800

    更新自动部署文件

--- public/webhook.php
+++ public/webhook.php
@@ -10,20 +10,19 @@ $path = "/www/wwwroot/docs.hub-sky.com/docs";
 
 $log = "/www/wwwroot/docs.hub-sky.com/docs/public/a.txt";
 file_put_contents($log, $_SERVER['HTTP_X_HUB_SIGNATURE'], FILE_APPEND);
+file_put_contents($log, file_get_contents("php://input"), FILE_APPEND);
 // Headers deliveried from GitHub
 try {
-    // $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 
-    // if ($signature) {
-    //     $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
-    //     if (strcmp($signature, $hash) == 0) {
-    //         echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-    //         exit();
-    //     }
-    // }
-    // shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-    `cd $path && git pull`;
-    exit();
+    if ($signature) {
+        $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+        if (strcmp($signature, $hash) == 0) {
+            file_put_contents($log, "执行指令", FILE_APPEND);
+            echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+            exit();
+        }
+    }
 } catch (\Throwable $th) {
     file_put_contents($log, $th->getMessage(), FILE_APPEND);
 }
