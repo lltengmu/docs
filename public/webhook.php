714d553f311b5c1f3fb07e3269c91a473e6c1846
eddie <1150970484@qq.com>
2025-01-09 22:16:53 +0800

    更新自动部署文件

--- public/webhook.php
+++ public/webhook.php
@@ -9,17 +9,27 @@ $secret = "Eddie-docs";
 $path = "/www/wwwroot/docs.hub-sky.com/docs";
 
 $log = "/www/wwwroot/docs.hub-sky.com/docs/public/a.txt";
-file_put_contents($log, $_SERVER['HTTP_X_HUB_SIGNATURE'] . "\n", FILE_APPEND);
 // Headers deliveried from GitHub
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+$command = "cd $path && /usr/bin/git pull";
+$output = [];
+$return_var = null;
 
 if ($signature) {
     $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
     if (strcmp($signature, $hash) == 0) {
-        file_put_contents($log, "执行指令" . "\n", FILE_APPEND);
         // $output = shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-        $output = shell_exec("cd {$path} && /usr/bin/git pull");
-        file_put_contents($log, "指令执行结果:" . $output . "\n", FILE_APPEND);
+        exec($command, $output, $return_var);
+        if ($return_var !== 0) {
+            // 命令执行失败，检查 $output 数组以获取错误信息
+            foreach ($output as $line) {
+                file_put_contents($log, "指令执行结果:" . $line . "\n", FILE_APPEND);
+            }
+        } else {
+            // 命令执行成功
+            file_put_contents($log, "Git operations completed successfully.\n", FILE_APPEND);
+        }
         exit();
     }
 }
